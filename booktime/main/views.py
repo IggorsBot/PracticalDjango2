275645e82c6711850e5c70169ec986b4fdb1a820
IggorsBot <IggorsBot@gmail.com>
2020-04-27 13:02:31 +0300

    add contact page

--- booktime/main/views.py
+++ booktime/main/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from main import forms
+from django.views.generic.edit import FormView
 
-# Create your views here.
+
+class ContactUsView(FormView):
+    template_name = "contact_form.html"
+    form_class = forms.ContactForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.send_mail()
+        return super().form_valid(form)
+
+# def contact_us(request):
+#     if request.method == 'POST':
+#         form = forms.ContactForm(request.POST)
+#         if form.is_valid():
+#             form.send_mail()
+#             return HttpResponseRedirect('/')
+#     else:
+#         form = forms.ContactForm()
+#     return render(request, 'contact_form.html', {'form': form})
